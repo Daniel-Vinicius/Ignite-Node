fd919033d6e23f69b83e98a342f5c2d778451111
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-06-07 17:25:38 -0300

    Criando token do usuário

--- 03-chapter/rentx/src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ 03-chapter/rentx/src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -6,6 +6,13 @@ import { CreateUserUseCase } from "./CreateUserUseCase";
 class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, email, password, driver_license } = request.body;
+
+    if (!email || !password || !driver_license || !name) {
+      return response.status(400).json({
+        error: "Fields email or password or name or driver_license missing!",
+      });
+    }
+
     const createUserUseCase = container.resolve(CreateUserUseCase);
 
     const data = {

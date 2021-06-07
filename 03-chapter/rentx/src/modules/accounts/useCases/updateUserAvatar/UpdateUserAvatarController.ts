dfdf96a300a1c6e723a3d1a806eab6c46b09600d
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-06-07 19:36:28 -0300

    Remover arquivo de avatar existente

--- 03-chapter/rentx/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ 03-chapter/rentx/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -6,7 +6,11 @@ import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
 class UpdateUserAvatarController {
   async handle(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
-    const avatar_file = request.file.filename;
+    const avatar_file = request.file?.filename;
+
+    if (!avatar_file) {
+      return response.status(400).json({ error: "Field avatar is required!" });
+    }
 
     const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
 

046875c92e2b8d28a151cfd71501ab587a0c6523
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-06-06 21:39:57 -0300

    03 Chapter

---
+++ 03-chapter/rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,55 @@
+import csvParse from "csv-parse";
+import fs from "fs";
+
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
+class ImportCategoryUseCase {
+  constructor(private categoriesRepository: CategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
+
+      const parseFile = csvParse();
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on("data", async (line) => {
+          const [categoryName, categoryDescription] = line;
+          categories.push({
+            name: categoryName,
+            description: categoryDescription,
+          });
+        })
+        .on("end", () => {
+          fs.promises.unlink(file.path);
+          resolve(categories);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    categories.map(async (category) => {
+      const { name, description } = category;
+
+      const existsCategory = this.categoriesRepository.findByName(name);
+      if (!existsCategory) {
+        this.categoriesRepository.create({
+          name,
+          description,
+        });
+      }
+    });
+  }
+}
+
+export { ImportCategoryUseCase };
---
+++ 03-chapter/rentalx/src/server.ts
@@ -0,0 +1,34 @@
+import express, { NextFunction, Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
+
+import AppError from "./errors/AppError";
+import { router } from "./routes";
+import swaggerFile from "./swagger.json";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(router);
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+
+  // eslint-disable-next-line no-console
+  console.error(err);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  });
+});
+
+app.listen(3333, () => console.log("Server is running!"));

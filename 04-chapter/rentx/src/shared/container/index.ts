d15fc2943f8ef2099c31c246d854d0614f508513
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-06-30 12:15:46 -0300

    Imports da aplicação

--- 04-chapter/rentx/src/shared/container/index.ts
+++ 04-chapter/rentx/src/shared/container/index.ts
@@ -1,13 +1,11 @@
-/* eslint-disable import-helpers/order-imports */
 import { container } from "tsyringe";
 
-import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
-import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
-import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
-
-import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
-import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
-import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { UsersRepository } from "@modules/accounts/repositories/implementations/UsersRepository";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
+import { CategoriesRepository } from "@modules/cars/repositories/implementations/CategoriesRepository";
+import { SpecificationsRepository } from "@modules/cars/repositories/implementations/SpecificationsRepository";
+import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",

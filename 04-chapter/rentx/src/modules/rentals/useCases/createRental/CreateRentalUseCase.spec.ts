2749b0272cc84cb7880a1d3da1130508d177b7ef
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-07-06 21:58:47 -0300

    Criando provider para data

--- 04-chapter/rentx/src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ 04-chapter/rentx/src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -2,17 +2,24 @@ import dayjs from "dayjs";
 
 import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
 import { rental as RentalFixture } from "@modules/rentals/test/fixtures/RentalFixture";
+import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
 import { CreateRentalUseCase } from "./CreateRentalUseCase";
 
 let createRentalUseCase: CreateRentalUseCase;
 let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+let dayJsProvider: DayjsDateProvider;
 
 describe("Create Rental", () => {
   beforeEach(() => {
+    dayJsProvider = new DayjsDateProvider();
     rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
-    createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+
+    createRentalUseCase = new CreateRentalUseCase(
+      rentalsRepositoryInMemory,
+      dayJsProvider
+    );
   });
 
   it("should be able to create a new rental", async () => {
